c3e8cb94f4763d85227ce8f3d0a0082a3a0cc278
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-14 21:53:38 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,8 +1,8 @@
-x=int(input())
-li=list(map(int,input().split(" ")))[:x]
-for i in range(0,len(li),1):
+x1=int(input())
+li1=list(map(int,input().split(" ")))[:x1]
+for i1 in range(0,len(li1),1):
    # m=li.index(li[i])
-    if(li.count(i)==1):
-        a=i
+    if(li1.count(i1)==1):
+        a1=i1
         
-print(a)
+print(a1)
