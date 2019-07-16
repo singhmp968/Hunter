fa1a105ddeaf430e302d8feab6e5d353c5f0f722
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-16 23:36:51 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,6 @@
+x=int(input())
+li=list(map(int,input().split()))[:x]
+for i in range(1,x,1):
+        if(i%2==0 and li[i]==0 or i%2!=0 and li[i]%2==0):
+                print(li[i],end=" ")
+                
