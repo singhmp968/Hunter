64c394b5a0dac91f94372f665d59f7f554fdead3
singhmp968 <31666974+singhmp968@users.noreply.github.com>
2019-07-17 21:57:48 +0530

    Update and rename Q12.py to Q121.py

--- Q12.py
+++
@@ -1,5 +0,0 @@
-x=map(str,input().split())
-for i in x:
-        
-        x=i[::-1]
-        print(x,end=" ")
---
+++ Q121.py
@@ -0,0 +1,5 @@
+x1=map(str,input().split())
+for i1 in x1:
+        
+        x1=i1[::-1]
+        print(x1,end=" ")
